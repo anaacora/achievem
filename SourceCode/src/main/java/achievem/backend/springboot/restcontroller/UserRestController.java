d7bda9d370b1db37faaceac893bf98c7d9319b1e
chrisZHAW <hofmach5@students.zhaw.ch>
2020-06-12 18:06:36 +0200

    just a commit

--- SourceCode/src/main/java/achievem/backend/springboot/restcontroller/UserRestController.java
+++ SourceCode/src/main/java/achievem/backend/springboot/restcontroller/UserRestController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,9 +16,10 @@ import achievem.backend.springboot.entities.Goal;
 import achievem.backend.springboot.entities.User;
 import achievem.backend.springboot.repositories.UserRepository;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 public class UserRestController {
-    
+	
     @Autowired
     private UserRepository repository;
     
@@ -45,6 +47,7 @@ public class UserRestController {
 	    }        
     }
     
+    @CrossOrigin("http://localhost:8081/")
     @RequestMapping(value = "/api/users/{id}", method = RequestMethod.GET)
     public ResponseEntity<User> getUserById(@PathVariable Long id){
     	
--- SourceCode/src/main/resources/public/src/assets/auth.js
+++ SourceCode/src/main/resources/public/src/assets/auth.js
@@ -2,7 +2,16 @@ import axios from 'axios';
 
 export function getUserId(id) {
 
-  axios.get('/api/users/'+id)
+  axios.get('http://localhost:8080/api/users/'+id,{
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST, GET, PUT, OPTIONS, DELETE',
+            'Access-Control-Allow-Headers': 'Access-Control-Allow-Methods, Access-Control-Allow-Origin, Origin, Accept, Content-Type',
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+    }
+  }
+  )
     .then((response)=>{
       console.log(response.data);
       return response.data;
@@ -11,6 +20,30 @@ export function getUserId(id) {
       console.log(error);
     })
 
+    axios.get('https://weng-message-board.herokuapp.com/api/messages')
+    .then( (res) => {
+      console.log(res.data);
+    })
+    .catch( (error) => {
+        console.log(error);
+    })
+
   // alert("hi Ana+Christian" + id)
+
+  // this.$http.get('http://localhost:8080/api/v1/users',
+  //     {
+  //         headers: {
+  //           'Access-Control-Allow-Origin': '*',
+  //           'Access-Control-Allow-Methods': 'POST, GET, PUT, OPTIONS, DELETE',
+  //           'Access-Control-Allow-Headers': 'Access-Control-Allow-Methods, Access-Control-Allow-Origin, Origin, Accept, Content-Type',
+  //           'Content-Type': 'application/json',
+  //           'Accept': 'application/json'
+  //         }
+  //       }
+  //       ).then(function (response) {
+  //         // Success
+  //       }, function (response) {
+  //         // Error
+  //       })
 }
 
